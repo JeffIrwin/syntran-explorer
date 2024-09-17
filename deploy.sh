319367fd25c55d28fa2f797593eb4cf4c555b457
Jeff Irwin <jirwin505@gmail.com>
2024-09-16 21:01:25 -0400

    set rows. will probably change later

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,16 @@ set -exu
 #set -ex
 
 git pull
+
+# If you ever start pm2 without prod env, you have to `pm2 kill` and restart to
+# reset the env
 export NODE_ENV="production"
 
 #pkill node || true
 ##nohup node server.js &
 #setsid nohup node server.js &
 
+# There's `pm2 restart` but it fails if you haven't already started :(
 pm2 stop server.js || true
 pm2 start server.js
 
