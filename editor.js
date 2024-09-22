56d8c93c8d0ee4ee4a542c5b8675cee932db75dc
Jeff Irwin <jirwin505@gmail.com>
2024-09-21 22:18:32 -0400

    formatting and whitespace

--- editor.js
+++ editor.js
@@ -4,63 +4,92 @@
 import { EditorState } from '@codemirror/state';
 
 import { highlightSelectionMatches } from '@codemirror/search';
-import { indentWithTab, history, defaultKeymap, historyKeymap } from '@codemirror/commands';
-import { foldGutter, indentOnInput, indentUnit, bracketMatching, foldKeymap, syntaxHighlighting, defaultHighlightStyle } from '@codemirror/language';
-import { closeBrackets, autocompletion, closeBracketsKeymap, completionKeymap } from '@codemirror/autocomplete';
-import { lineNumbers, highlightActiveLineGutter, highlightSpecialChars, drawSelection, dropCursor, rectangularSelection, crosshairCursor, highlightActiveLine, keymap, EditorView } from '@codemirror/view';
+import {
+	indentWithTab,
+	history,
+	defaultKeymap,
+	historyKeymap
+} from '@codemirror/commands';
+
+import {
+	foldGutter,
+	indentOnInput,
+	indentUnit,
+	bracketMatching,
+	foldKeymap,
+	syntaxHighlighting,
+	defaultHighlightStyle
+} from '@codemirror/language';
+
+import {
+	closeBrackets,
+	autocompletion,
+	closeBracketsKeymap,
+	completionKeymap
+} from '@codemirror/autocomplete';
+
+import {
+	lineNumbers,
+	highlightActiveLineGutter,
+	highlightSpecialChars,
+	drawSelection,
+	dropCursor,
+	rectangularSelection,
+	crosshairCursor,
+	highlightActiveLine,
+	keymap,
+	EditorView
+} from '@codemirror/view';
 
 // Theme
 import { oneDark } from "@codemirror/theme-one-dark";
 
 // Language
 import { rust } from "@codemirror/lang-rust";
-//import { javascript } from "@codemirror/lang-javascript";
 
 function createEditorState(initialContents, options = {}) {
-    let extensions = [
-        lineNumbers(),
-        EditorView.lineWrapping,
-        highlightActiveLineGutter(),
-        highlightSpecialChars(),
-        history(),
-        foldGutter(),
-        drawSelection(),
-        indentUnit.of("    "),
-        EditorState.allowMultipleSelections.of(true),
-        indentOnInput(),
-        bracketMatching(),
-        closeBrackets(),
-        autocompletion(),
-        rectangularSelection(),
-        crosshairCursor(),
-        highlightActiveLine(),
-        highlightSelectionMatches(),
-        keymap.of([
-            indentWithTab,
-            ...closeBracketsKeymap,
-            ...defaultKeymap,
-            ...historyKeymap,
-            ...foldKeymap,
-            ...completionKeymap,
-        ]),
-        rust(),
-        //javascript(),
-        syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
-    ];
+	let extensions = [
+		lineNumbers(),
+		EditorView.lineWrapping,
+		highlightActiveLineGutter(),
+		highlightSpecialChars(),
+		history(),
+		foldGutter(),
+		drawSelection(),
+		indentUnit.of("    "),
+		EditorState.allowMultipleSelections.of(true),
+		indentOnInput(),
+		bracketMatching(),
+		closeBrackets(),
+		autocompletion(),
+		rectangularSelection(),
+		crosshairCursor(),
+		highlightActiveLine(),
+		highlightSelectionMatches(),
+		keymap.of([
+			indentWithTab,
+			...closeBracketsKeymap,
+			...defaultKeymap,
+			...historyKeymap,
+			...foldKeymap,
+			...completionKeymap,
+		]),
+		rust(),
+		syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
+	];
 
-    if (options.oneDark)
-        extensions.push(oneDark);
+	if (options.oneDark)
+		extensions.push(oneDark);
 
-    return EditorState.create({
-        doc: initialContents,
-        extensions
-    });
+	return EditorState.create({
+		doc: initialContents,
+		extensions
+	});
 }
 
 function createEditorView(state, parent) {
-    return new EditorView({
+	return new EditorView({
 		state,
-		//lineWrapping: true,
 		parent
 	});
 }
